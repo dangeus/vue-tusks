bc12e465c6eb6203356acd18e7a9301042f8244b
dangeus <bjokugan@mail.ru>
2024-12-29 10:56:29 +0300

    Сделал продвинутые задания

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
 <script setup>
-import {ref} from 'vue'
-
-const count = ref(0)
-
-function increment() {
-  count.value++
-}
-
-function decrement(){
-  count.value--
-}
+import NewComponent from '@/components/NewComponent.vue'
 </script>
 
 <template>
-  <span class="p-4 accent-red-300">{{ count }}</span>
-  <button @click="increment" class="bg-green-300">Увеличить</button>
-  <button @click="decrement" class="bg-red-300">Уменьшить</button>
+  <NewComponent/>
 </template>
 
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref, watch, reactive} from 'vue'
+
+const inputValue = ref(``)
+const user = reactive({
+  name: 'Maxim',
+  age: 20,
+})
+const newName = ref(``)
+
+function changeUserName(){
+  user.name = newName.value
+}
+
+watch(inputValue, () => {
+  console.log(`Новое значение Value = ${inputValue.value}`)
+})
+
+</script>
+
+<template>
+  <input v-model="inputValue" class="border" placeholder="Напишите что-нибудь">
+  <div>Введенное значение: {{ inputValue }}</div>
+  <div>
+    <p>Информация о пользователе {{ user.name }} следующая: его возраст - {{ user.age }}</p>
+    <input v-model="newName" placeholder="Новое имя">
+    <button @click="changeUserName" class="px-4 bg-green-500">Поменять имя пользователя</button>
+  </div>
+</template>
+
