5fc539bf8a162cb9b12ad90533def20ef178cf6c
dangeus <bjokugan@mail.ru>
2024-12-29 10:17:37 +0300

    Сделал базовые задания

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
-<script>
+<script setup>
+import {ref} from 'vue'
 
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+function decrement(){
+  count.value--
+}
 </script>
 
 <template>
-  <div class="font-bold">Hello world</div>
+  <span class="p-4 accent-red-300">{{ count }}</span>
+  <button @click="increment" class="bg-green-300">Увеличить</button>
+  <button @click="decrement" class="bg-red-300">Уменьшить</button>
 </template>
 
